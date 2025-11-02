abca084f1dc99b8d95313577911aa1a92de0e83f
Evan Durfee <edurfee@pm.me>
2025-11-02 12:05:07 +0100

    Fix alignment and comments for linter

--- Casks/jetbrains-pycharm.rb
+++ Casks/jetbrains-pycharm.rb
@@ -1,12 +1,8 @@
 cask "jetbrains-pycharm" do
   arch arm64_linux: "-aarch64"
-  # https://www.jetbrains.com/pycharm/download/other.html
-  # cksum -a sha256 <file>
-  # or at https://download.jetbrains.com/python/pycharm-2025.2.4.tar.gz.sha256
-  # Also ref https://github.com/Homebrew/homebrew-cask/blob/143bed19c5895fed23f87bf30f0f1865c2e689ae/Casks/i/intellij-idea.rb
   version "2025.2.4,252.27397.106"
   sha256 x86_64_linux: "f1613a171ab07ba0e7ccd13cb537af4da8920c89cf4a13c64a3f7ae72e803701",
-         arm64_linux: "91569e7ba9988af1aa4d9973f52dfa8af8a94f32a53faa8457c7ba17d25f43b3"
+         arm64_linux:  "91569e7ba9988af1aa4d9973f52dfa8af8a94f32a53faa8457c7ba17d25f43b3"
 
   url "https://download.jetbrains.com/python/pycharm-#{version.csv.first}#{arch}.tar.gz"
   name "PyCharm"
@@ -14,7 +10,6 @@ cask "jetbrains-pycharm" do
   homepage "https://www.jetbrains.com/pycharm/"
 
   livecheck do
-    # curl ... | jq '.PCP[0] | .version, .build'
     url "https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release"
     strategy :json do |json|
       json["PCP"]&.map do |release|
@@ -42,9 +37,6 @@ cask "jetbrains-pycharm" do
   preflight do
     FileUtils.mkdir_p("#{Dir.home}/.local/share/applications")
     FileUtils.mkdir_p("#{Dir.home}/.local/share/icons")
-    # We need this file to start, but Jetbrains Toolbox will overwrite it on its first run with a proper one
-    # It will also extract the icon from somewhere, but it doesn't exist until first run, so we just point to where it
-    # should be, and it will get fixed on first run
     File.write("#{staged_path}/pycharm.desktop", <<~EOS)
       [Desktop Entry]
       Name=PyCharm
