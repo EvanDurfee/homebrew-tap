222db3a05d18af4f70f5f235046cbc9e24c26668
Evan Durfee <20136084+EvanDurfee@users.noreply.github.com>
2025-11-11 23:21:18 +0000

    jetbrains-rider 2025.3

--- Casks/jetbrains-rider.rb
+++ Casks/jetbrains-rider.rb
@@ -3,9 +3,9 @@ cask "jetbrains-rider" do
        arm:   "-aarch64"
   os linux: "linux"
 
-  version "2025.2.4,252.27397.121"
-  sha256 x86_64_linux: "cd428f7d6db5055cd2594c3d1ef91241843d263cb6301369d3121a847ffb6589",
-         arm64_linux:  "75c38a2bd94e31200fc4e3a6cfad83b35c65d36c9d61b9a7b208e3ec82f8c81a"
+  version "2025.3,253.28294.88"
+  sha256 x86_64_linux: "bf7be76634ec70dc33008505a08dcc024d4d7ec2bf0e4ace8a71f219d2f40706",
+         arm64_linux:  "bce1eed1a6fac3cc0850fc2b38ef557e1c51872c4b619a86ce36d002f6cbcbcb"
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.csv.first}#{arch}.tar.gz"
   name "Rider"
