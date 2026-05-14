5aeea6d8513a674ee8ec706509ec504df836d856
Evan Durfee <edurfee@pm.me>
2026-05-14 17:48:04 +0200

    Add zed cask

---
+++ Casks/zed-linux.rb
@@ -0,0 +1,46 @@
+cask "zed-linux" do
+  version "1.2.3"
+  sha256 "8ff7e271b9918753bc857b777b2b45ecf9dafe8014fa7aa1efc0817bee5485dc"
+
+  url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
+  name "Zed"
+  desc "High-performance, multiplayer code editor"
+  homepage "https://zed.dev/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "zed.app/bin/zed"
+
+  preflight do
+    xdg_data = ENV.fetch("XDG_DATA_HOME", "#{Dir.home}/.local/share")
+    FileUtils.mkdir_p "#{xdg_data}/applications"
+    FileUtils.mkdir_p "#{xdg_data}/icons"
+  end
+
+  postflight do
+    xdg_data = ENV.fetch("XDG_DATA_HOME", "#{Dir.home}/.local/share")
+    desktop_content = File.read("#{staged_path}/zed.app/share/applications/dev.zed.Zed.desktop")
+    desktop_content.gsub!(/^TryExec=.*/, "TryExec=#{HOMEBREW_PREFIX}/bin/zed")
+    desktop_content.gsub!(/^Exec=zed/, "Exec=#{HOMEBREW_PREFIX}/bin/zed")
+    desktop_content.gsub!(/^Icon=.*/, "Icon=zed")
+    File.write("#{xdg_data}/applications/dev.zed.Zed.desktop", desktop_content)
+    FileUtils.cp("#{staged_path}/zed.app/share/icons/hicolor/512x512/apps/zed.png",
+                 "#{xdg_data}/icons/hicolor/512x512/apps/zed.png")
+    system "/usr/bin/xdg-icon-resource", "forceupdate"
+  end
+
+  uninstall_postflight do
+    xdg_data = ENV.fetch("XDG_DATA_HOME", "#{Dir.home}/.local/share")
+    FileUtils.rm("#{xdg_data}/applications/dev.zed.Zed.desktop")
+    FileUtils.rm("#{xdg_data}/icons/hicolor/512x512/apps/zed.png")
+  end
+
+  zap trash: [
+    "#{ENV.fetch("XDG_CACHE_HOME", "#{Dir.home}/.cache")}/zed",
+    "#{ENV.fetch("XDG_CONFIG_HOME", "#{Dir.home}/.config")}/zed",
+    "#{ENV.fetch("XDG_DATA_HOME", "#{Dir.home}/.local/share")}/zed",
+  ]
+end
