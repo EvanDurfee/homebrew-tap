25cd91080e773531aa57ed753304fdd36fe2ba16
Evan Durfee <20136084+EvanDurfee@users.noreply.github.com>
2026-05-29 06:49:05 +0000

    zed-linux 1.4.4

--- Casks/zed-linux.rb
+++ Casks/zed-linux.rb
@@ -1,6 +1,6 @@
 cask "zed-linux" do
-  version "1.3.6"
-  sha256 "5c152c229d689ed7aaa441aadb15c920351eee7cabfa938910ea66f654a60b67"
+  version "1.4.4"
+  sha256 "e5c80658824c47f1cf28ed73441cf26a5d1d62807bafcc0385cf239298e39cf0"
 
   url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
   name "Zed"
